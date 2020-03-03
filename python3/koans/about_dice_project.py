c1694454b0c6202284f8bc6659e93afc22a20f16
mrrusik <mrrusik2012@gmail.com>
2020-03-03 20:23:22 +0200

    bindings

--- python3/koans/about_dice_project.py
+++ python3/koans/about_dice_project.py
@@ -14,9 +14,20 @@ class DiceSet:
         return self._values
 
     def roll(self, n):
+        # Tip: random.randint(min, max) can be used to generate random numbers
+        self._values = []
+        for r in range(0, n):
+          self._values.append(random.randint(1, 6))
+
+    def roll(self, n):
         # Needs implementing!
         # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = []
+        for r in range(0, n):
+            self._values.append(random.randint(1,6))
+
+
+
 
 class AboutDiceProject(Koan):
     def test_can_create_a_dice_set(self):
